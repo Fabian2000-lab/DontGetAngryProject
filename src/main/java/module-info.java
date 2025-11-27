769d59cd2cf09e2a59abad8ca2ca34b4edfb8556
fabia <fabian.huonker@zgs-konstanz.de>
2025-11-27 11:42:06 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module zgkprojekt.dontgetangry {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens zgkprojekt.dontgetangry to javafx.fxml;
+    exports zgkprojekt.dontgetangry;
+}
---
+++ src/main/java/zgkprojekt/dontgetangry/Launcher.java
@@ -0,0 +1,9 @@
+package zgkprojekt.dontgetangry;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
