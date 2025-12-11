7dde525d38325f605ee255e6dd2d1dbcd35a0ec9
fabia <fabian.huonker@zgs-konstanz.de>
2025-12-11 11:40:25 +0100

    added MainController.java and MainService.java

---
+++ src/main/java/zgkprojekt/service/MainService.java
@@ -0,0 +1,45 @@
+package zgkprojekt.service;
+
+import zgkprojekt.model.PlayingField;
+
+public class MainService {
+
+    private static MainService _instance;
+
+    private PlayingField _playingField;
+
+    private MainService()
+    {
+        _playingField = new PlayingField();
+    }
+
+    public static MainService getInstance()
+    {
+        if(_instance == null)
+            _instance = new MainService();
+
+        return _instance;
+    }
+
+    public void mapEvent() {
+    }
+
+    public void useItemOptional() {
+    }
+
+    public void useDice() {
+    }
+
+    public void makeFigureMove() {
+    }
+
+    public void triggerEventFieldCheck() {
+    }
+
+    public boolean checkForWinner() {
+        return false;
+    }
+
+    public void saveDataToDataBase() {
+    }
+}
